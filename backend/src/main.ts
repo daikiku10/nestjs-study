306251276383ebede08825a7dec32b27573e9d17
daikiku10 <soccerdk19.910@gmail.com>
2025-02-15 16:18:00 +0900

    CORS設定 GET

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET',
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
