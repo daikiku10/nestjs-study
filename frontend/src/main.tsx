b1dc2dc95f93f43fd97c8a2de2ad51f384ccef9e
daikiku10 <soccerdk19.910@gmail.com>
2025-02-23 07:40:37 +0900

    type-checkの追加

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { ApolloClientProvider } from "./libs/apollo-client.tsx";
+import App from "./App";
+import { ApolloClientProvider } from "./libs/apollo-client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
