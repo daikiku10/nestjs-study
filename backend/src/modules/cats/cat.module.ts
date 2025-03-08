60e577d41c11394f6add05d04bede6316200520e
daikiku10 <soccerdk19.910@gmail.com>
2025-03-08 13:47:36 +0900

    resovler修正 codefirst

--- backend/src/modules/cats/cat.module.ts
+++ backend/src/modules/cats/cat.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CatSchema, Cat } from './repository/cat.schema';
 import { CatsService } from './application/cats.service';
-import { CatsResolver } from './interfaces/cat.resolver';
+import { CatsResolver } from './cat.resolver';
 import { CatsRepository } from './repository/cat.repository';
 
 @Module({
---
+++ backend/src/modules/cats/cat.resolver.ts
@@ -0,0 +1,89 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CatsService } from './application/cats.service';
+import { CatModel } from './interfaces/cat.model';
+
+@Resolver()
+export class CatsResolver {
+  constructor(private readonly catsService: CatsService) {
+    console.log('CatsResolver生成', catsService);
+  }
+
+  @Query(() => CatModel)
+  getAllCats(): CatModel {
+    // service層へ依頼
+    // ドメインからモデルへ変換
+    return { id: 'daiki', name: 'daiki', age: 12 };
+  }
+  // try {
+  //   const result = await this.catsService.findAll();
+  //   let res: Cat[] = [];
+  //   result.forEach((item) => {
+  //     const cat = {
+  //       id: item.id,
+  //       name: item.name,
+  //       age: item.age,
+  //       breed: item.breed,
+  //     } satisfies Cat;
+  //     res = [...res, cat];
+  //   });
+  //   return res;
+  // } catch (error) {
+  //   console.log('error', error);
+  //   return undefined;
+  // }
+
+  // @Query(() => Object)
+  // async getCatById(@Args('id') id: string) {
+  //   try {
+  //     const result = await this.catsService.findCatById(id);
+  //     const cat = {
+  //       id: result.id,
+  //       name: result.name,
+  //       age: result.age,
+  //       breed: result.breed,
+  //     } satisfies Cat;
+  //     return cat;
+  //   } catch (error) {
+  //     console.log('error', error);
+  //     return undefined;
+  //   }
+  // }
+  @Mutation(() => String)
+  async createCat(
+    @Args('name') name: string,
+    @Args('age') age: number,
+  ): Promise<string> {
+    console.log('リクエスト');
+    try {
+      const result = await this.catsService.create(name, age, 'スコティッシュ');
+      console.log(result);
+      return '成功';
+    } catch (error) {
+      console.log('error', error);
+      return error;
+    }
+  }
+  // @Mutation(() => Object)
+  // async updateCat(
+  //   @Args('id') id: string,
+  //   @Args('name') name: string,
+  //   @Args('age') age: number,
+  //   @Args('breed') breed: string,
+  // ) {
+  //   try {
+  //     const result = await this.catsService.update(id, name, age, breed);
+  //     return result;
+  //   } catch (error) {
+  //     return error;
+  //   }
+  // }
+  // @Mutation()
+  // async deleteCat(@Args('id') id: string) {
+  //   try {
+  //     await this.catsService.deleteCatById(id);
+  //     return;
+  //   } catch (error) {
+  //     return error;
+  //   }
+  // }
+}
