7b34c7802e15cc7fbab686703d86f5b19f54abda
daikiku10 <soccerdk19.910@gmail.com>
2025-03-08 13:48:15 +0900

    service修正 codefirst

--- backend/src/modules/cats/application/cats.service.ts
+++ backend/src/modules/cats/application/cats.service.ts
@@ -4,43 +4,39 @@ import { CatsRepository } from '../repository/cat.repository';
 
 @Injectable() // アプリケーション層
 export class CatsService {
-  constructor(private readonly catsRepo: CatsRepository) {}
+  constructor(private readonly catsRepository: CatsRepository) {
+    console.log('CatsService生成');
+  }
 
   async create(name: string, age: number, breed: string) {
     // 受け取ったデータを使用してドメイン変換
     const catDomain = CatDomain.create(name, age, breed);
-
     // リポジトリ層へ依頼
-    return this.catsRepo.insert(catDomain);
+    return this.catsRepository.insert(catDomain);
   }
-
-  async update(id: string, name: string, age: number, breed: string) {
-    // 受け取ったデータを使用してドメイン変換
-    const catDomain = CatDomain.createForRepository(id, name, age, breed);
-
-    // リポジトリ層へ依頼
-    try {
-      return this.catsRepo.update(catDomain);
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async findAll() {
-    // リポジトリ層へ依頼
-    const result = await this.catsRepo.findAll();
-    return result;
-  }
-
-  async findCatById(id: string) {
-    // リポジトリ層へ依頼
-    const result = await this.catsRepo.findCatById(id);
-    return result;
-  }
-
-  async deleteCatById(id: string) {
-    // リポジトリ層へ依頼
-    await this.catsRepo.delete(id);
-    return;
-  }
+  // async update(id: string, name: string, age: number, breed: string) {
+  //   // 受け取ったデータを使用してドメイン変換
+  //   const catDomain = CatDomain.createForRepository(id, name, age, breed);
+  //   // リポジトリ層へ依頼
+  //   try {
+  //     return this.catsRepo.update(catDomain);
+  //   } catch (e) {
+  //     throw new Error(e);
+  //   }
+  // }
+  // async findAll() {
+  //   // リポジトリ層へ依頼
+  //   const result = await this.catsRepo.findAll();
+  //   return result;
+  // }
+  // async findCatById(id: string) {
+  //   // リポジトリ層へ依頼
+  //   const result = await this.catsRepo.findCatById(id);
+  //   return result;
+  // }
+  // async deleteCatById(id: string) {
+  //   // リポジトリ層へ依頼
+  //   await this.catsRepo.delete(id);
+  //   return;
+  // }
 }
