b6ee9ec8d71e1259424b0598bdc5ed17a482af3c
daikiku10 <soccerdk19.910@gmail.com>
2025-03-08 13:48:46 +0900

    repository修正 codefirst

--- backend/src/modules/cats/repository/cat.repository.ts
+++ backend/src/modules/cats/repository/cat.repository.ts
@@ -6,57 +6,56 @@ import { Model } from 'mongoose';
 
 @Injectable() // リポジトリ層
 export class CatsRepository {
-  constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
-
+  constructor(@InjectModel(Cat.name) private catDocument: Model<Cat>) {
+    console.log(Cat.name);
+    console.log(catDocument);
+    console.log('CatsRepository生成');
+  }
   insert(cat: CatType) {
-    const createdCat = new this.catModel(cat);
+    const createdCat = new this.catDocument(cat);
     return createdCat.save();
   }
-
-  async update(cat: CatType) {
-    const updateObj = await this.catModel.findOne({ id: cat.id }).exec();
-    if (!updateObj) {
-      throw new Error('更新対象が見つかりませんでした。');
-    }
-    await updateObj
-      .updateOne({
-        name: cat.name,
-        age: cat.age,
-        breed: cat.breed,
-      })
-      .exec();
-    return updateObj;
-  }
-
-  async findAll() {
-    const result = await this.catModel.find().exec();
-    let res: CatType[] = [];
-    // ドメイン変換
-    result.forEach((item) => {
-      const cat = CatDomain.createForRepository(
-        item.id,
-        item.name,
-        item.age,
-        item.breed,
-      );
-      res = [...res, cat];
-    });
-    return res;
-  }
-
-  async findCatById(id: string) {
-    const result = await this.catModel.findOne({ id: id }).exec();
-    // ドメイン変換
-    const res = CatDomain.createForRepository(
-      result.id,
-      result.name,
-      result.age,
-      result.breed,
-    );
-    return res;
-  }
-
-  async delete(id: string) {
-    this.catModel.deleteOne({ id: id }).exec();
-  }
+  //   async update(cat: CatType) {
+  //     const updateObj = await this.catModel.findOne({ id: cat.id }).exec();
+  //     if (!updateObj) {
+  //       throw new Error('更新対象が見つかりませんでした。');
+  //     }
+  //     await updateObj
+  //       .updateOne({
+  //         name: cat.name,
+  //         age: cat.age,
+  //         breed: cat.breed,
+  //       })
+  //       .exec();
+  //     return updateObj;
+  //   }
+  //   async findAll() {
+  //     const result = await this.catModel.find().exec();
+  //     let res: CatType[] = [];
+  //     // ドメイン変換
+  //     result.forEach((item) => {
+  //       const cat = CatDomain.createForRepository(
+  //         item.id,
+  //         item.name,
+  //         item.age,
+  //         item.breed,
+  //       );
+  //       res = [...res, cat];
+  //     });
+  //     return res;
+  //   }
+  //   async findCatById(id: string) {
+  //     const result = await this.catModel.findOne({ id: id }).exec();
+  //     // ドメイン変換
+  //     const res = CatDomain.createForRepository(
+  //       result.id,
+  //       result.name,
+  //       result.age,
+  //       result.breed,
+  //     );
+  //     return res;
+  //   }
+  //   async delete(id: string) {
+  //     this.catModel.deleteOne({ id: id }).exec();
+  //   }
 }
