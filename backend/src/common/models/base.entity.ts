09922808d899cf4b5b7b3eb7ef899790bdb807df
daikiku10 <soccerdk19.910@gmail.com>
2025-04-16 15:42:18 +0900

    baseEntityの作成

---
+++ backend/src/common/models/base.entity.ts
@@ -0,0 +1,38 @@
+import { HideField, ObjectType } from '@nestjs/graphql';
+import { BaseModel } from './base.model';
+import { Optional } from 'utility-types';
+import { ObjectIdColumn } from 'typeorm';
+import { IdColumn } from '../decorators/property/id-column.decorator';
+import { generateUniqId } from '../utils/id/generateUniqId';
+
+@ObjectType({
+  isAbstract: true,
+})
+export class BaseEntity<T = any> extends BaseModel {
+  constructor(
+    props?: Optional<BaseEntity & T>,
+    id?: (props: Optional<BaseEntity & T>) => { keys: string | string[] },
+  ) {
+    console.log('id', id); // TODO: 使用する
+    super(props);
+    if (props) {
+      this._id = this.id = props.id || generateUniqId();
+    }
+    // TODO: deletedAtの追加
+  }
+
+  /**
+   * _ID
+   */
+  @ObjectIdColumn({ type: 'string' })
+  @HideField()
+  _id: any; // TODO: anyでリントエラーになるようにと、ApiHidePropertyの追加
+
+  /**
+   * ID
+   */
+  @IdColumn({
+    description: 'ID',
+  })
+  id: string;
+}
