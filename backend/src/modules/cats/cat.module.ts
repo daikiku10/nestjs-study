642882d8919e26469ec9c3076868cbf33c2783e3
daikiku10 <soccerdk19.910@gmail.com>
2025-05-04 12:27:05 +0900

    不要なコードの削除

--- backend/src/modules/cats/cat.module.ts
+++ backend/src/modules/cats/cat.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatSchema, Cat } from './repository/cat.schema';
 import { CatsResolver } from './cat.resolver';
-import { CatsRepository } from './repository/cat.repository';
-import { CatDaService } from './service/cat.service';
 import usecase from './usecase';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import entities from './entities';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
-    TypeOrmModule.forFeature([...entities]),
-  ],
-  providers: [CatsResolver, ...usecase, CatsRepository, CatDaService],
+  imports: [TypeOrmModule.forFeature([...entities])],
+  providers: [CatsResolver, ...usecase],
 })
 export class CatsModule {}
--- backend/src/modules/cats/service/cat.service.ts
+++
@@ -1,9 +0,0 @@
-export class CatDaService {
-  constructor() {
-    console.log('CatDaService生成');
-  }
-
-  execute() {
-    console.log('service実行');
-  }
-}
