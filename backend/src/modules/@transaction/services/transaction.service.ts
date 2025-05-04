c6d0863c64e8149b3d42e0d981374828f704736c
daikiku10 <soccerdk19.910@gmail.com>
2025-05-05 08:49:55 +0900

    Transaction処理の実装

--- backend/src/modules/@transaction/services/transaction.service.ts
+++ backend/src/modules/@transaction/services/transaction.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { MongoEntityManager } from 'typeorm';
 
 @Injectable()
 export class TransactionService {
-  constructor() {}
+  constructor(
+    @InjectEntityManager()
+    private readonly entityManager: MongoEntityManager,
+  ) {}
+
+  // TODO: private sessions = new Map<string, ClientSession>();
+
+  async begin(): Promise<void> {
+    // TODO: セッションの再利用を試みる(別々で呼ばれた場合に、同じトランザクションにならない)
+    // セッションの作成
+    const session =
+      this.entityManager.mongoQueryRunner.databaseConnection.startSession();
+
+    const isTopTransaction = !session.inTransaction();
+
+    if (isTopTransaction) {
+      session.startTransaction();
+      console.log('トップレベルのトランザクションを開始しました。');
+    } else {
+      console.log('すでにトランザクションが開始されています。');
+    }
+
+    try {
+      if (isTopTransaction) {
+        await session.commitTransaction();
+      }
+    } catch (e) {
+      if (isTopTransaction) {
+        await session.abortTransaction();
+      }
+      throw e;
+    } finally {
+      if (isTopTransaction) {
+        await session.endSession();
+      }
+    }
+  }
 }
