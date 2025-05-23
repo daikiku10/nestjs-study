53834c754fbd9aa1a8ffe7fe347ffbb286b61871
daikiku10 <soccerdk19.910@gmail.com>
2025-05-24 08:39:40 +0900

    transactionModuleの作成

--- backend/src/modules/@transaction/services/transaction.service.ts
+++ backend/src/modules/@transaction/services/transaction.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
-import { MongoEntityManager } from 'typeorm';
+import { bulkSave } from 'src/common/utils/typeorm/bulkSave';
+import { bulkSoftRemove } from 'src/common/utils/typeorm/bulkSoftRemove';
+import { MongoEntityManager, MongoRepository } from 'typeorm';
+
+type BulkOperations = {
+  bulkSave: typeof bulkSave;
+  bulkSoftRemove: typeof bulkSoftRemove;
+};
+
+type TransactionFunction = (props: BulkOperations) => Promise<void>;
 
 @Injectable()
 export class TransactionService {
@@ -11,7 +20,7 @@ export class TransactionService {
 
   // TODO: private sessions = new Map<string, ClientSession>();
 
-  async begin(): Promise<void> {
+  async begin(func: TransactionFunction): Promise<void> {
     // TODO: セッションの再利用を試みる(別々で呼ばれた場合に、同じトランザクションにならない)
     // セッションの作成
     const session =
@@ -27,6 +36,8 @@ export class TransactionService {
     }
 
     try {
+      const runner = new BulkOperationRunner(this.entityManager);
+      await func(runner.getBulkOperations());
       if (isTopTransaction) {
         await session.commitTransaction();
       }
@@ -41,4 +52,31 @@ export class TransactionService {
       }
     }
   }
+}
+
+class BulkOperationRunner {
+  constructor(private readonly entityManager: MongoEntityManager) {}
+
+  public getBulkOperations(): BulkOperations {
+    return {
+      bulkSave: this.bulkSave.bind(this),
+      bulkSoftRemove: this.bulkSoftRemove.bind(this),
+    };
+  }
+
+  async bulkSave<Entity>(
+    repository: MongoRepository<Entity>,
+    entities: Entity[],
+  ) {
+    await bulkSave(repository, entities);
+  }
+
+  async bulkSoftRemove(repository: MongoRepository<any>, entities: any[]) {
+    await bulkSoftRemove(repository, entities);
+  }
+
+  // NOTE: EntityTargetの場合は必要になってくる？
+  // private getRepository<Entity>(repository: MongoRepository<Entity>) {
+  //   return this.entityManager.getMongoRepository(repository.target);
+  // }
 }
--- backend/src/modules/@transaction/transaction.module.ts
+++ backend/src/modules/@transaction/transaction.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import services from './services';
 
 @Module({
+  imports: [],
   providers: [...services],
+  exports: [...services],
 })
 export class TransactionModule {}
