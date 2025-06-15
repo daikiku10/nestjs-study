be85342b2e0ec56494ae1a4c7d5ece4d1c65752a
daikiku10 <soccerdk19.910@gmail.com>
2025-06-15 22:03:21 +0900

    縦の区切り線

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,13 @@ function App() {
       <div className="flex items-center justify-center">
         <ChipPathButton />
       </div>
+
+      <div className="flex items-center justify-center">
+        <section className="section">
+          <div className="section__item section__item--start">Content1</div>
+          <div className="section__item">Content2</div>
+        </section>
+      </div>
     </div>
   );
 }
