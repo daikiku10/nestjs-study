6b940966702946881658da49db371658f605b725
daikiku10 <soccerdk19.910@gmail.com>
2025-06-20 23:32:54 +0900

    アクセシビリティの改善

--- frontend/components/ChipPathButton.tsx
+++ frontend/components/ChipPathButton.tsx
@@ -2,10 +2,10 @@ import TrashIcon from "./icons/TrashIcon";
 
 export default function ChipPathButton() {
   return (
-    <button className="button cursor-pointer relative flex items-center gap-[8px] h-[40px] rounded-[9999px] bg-[#f6f5f5] px-[24px] font-medium text-[#211c20] select-none">
+    <button className="button cursor-pointer relative flex items-center gap-[8px] h-[40px] rounded-[9999px] bg-gray-300 px-[24px] font-medium text-black select-none">
       <div
         aria-hidden="true"
-        className="hold-overlay absolute inset-0 flex items-center justify-center gap-2 rounded-[9999px] bg-[#ffdbdc] text-[#e5484d]"
+        className="hold-overlay absolute inset-0 flex items-center justify-center gap-2 rounded-[9999px] bg-[#ffdbdc] text-red-700"
       >
         <TrashIcon />
         長押しで削除
